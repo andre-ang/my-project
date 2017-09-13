eeab36ecf5cd5b9167247459bb55238756d99f85
= <=>
2017-09-13 14:50:19 +0800

    Change Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My project
                 </div>
 
                 <div class="links">
